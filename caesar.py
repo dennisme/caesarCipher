f1da51767a9b5537051c473893f19b020792f38a
Matthew Dennison <medennison123@gmail.com>
2014-09-21 19:41:39 -0400

    fixed argparse issues

--- caesar.py
+++ caesar.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python
 
+"""
+Matthew Dennison
+NCS 541
+Assignment 1
+
+"""
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("square", type=int,
-        help="display a square of a given numnber")
-parser.add_argument("-v","--verbose", help="increase output verbosity",
-        action="store_true")
-args = parser.parse_args()
-answer = args.square**2
-if args.verbose:
-    print "the square of {} equals {}".format(args.square, answer)
-else:
-    print answer 
+parser = argparse.ArgumentParser(description="Caesar Cipher")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-e", "--encrypt", action="store_true",
+       help="Encrypt plaintext")
+group.add_argument("-d", "--decrypt", action="store_true",
+       help="Decrypt cyphertext" )
+parser.add_argument("-s","--string", 
+        help="String to be encrypted or decryped")
 
 
-
-
+if __name__ == '__main__':
+    args = parser.parse_args()
+    if args.encrypt:
+        encrypt_plaintext(args)
+    elif args.decrypt:
+        decrypt_plaintext(args)
+    else:
+        print "Error, please see usage or --help for assistance"
