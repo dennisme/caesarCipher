2904b6dafbde23f46ae72894e6f40dc999576a49
Matthew Dennison <medennison123@gmail.com>
2014-09-21 20:53:32 -0400

    checking logic and fixing errors

--- caesar.py
+++ caesar.py
@@ -11,18 +11,41 @@ import argparse
 parser = argparse.ArgumentParser(description="Caesar Cipher")
 group = parser.add_mutually_exclusive_group()
 group.add_argument("-e", "--encrypt", action="store_true",
-       help="Encrypt plaintext")
+       help="Encrypt Alice's plaintext for Bob")
 group.add_argument("-d", "--decrypt", action="store_true",
-       help="Decrypt cyphertext" )
+       help="Bob decrypting Alice's cyphertext" )
+group.add_argument("-c", "--charlie", action="store_true", 
+        help="Charlie's intercepted cyphertext")
+group.add_argument("-diff", "--difference", action="store_true",
+      help="Decrypting Charlie's modified cyphertext")
 parser.add_argument("-s","--string", 
         help="String to be encrypted or decryped")
 
+def encrypt_plaintext(args):
+    print "encrypt works"
+    print args.string
+
+def decrypt_cyphertext(args):
+    print "decrypt works"
+    print args.string
+
+def decrypt_intercepted(args):
+    print "charlie works"
+    print args.string
+
+def compare_intercepted(args):
+    print "diff works"
+    print args.string
 
 if __name__ == '__main__':
     args = parser.parse_args()
     if args.encrypt:
         encrypt_plaintext(args)
     elif args.decrypt:
-        decrypt_plaintext(args)
+        decrypt_cyphertext(args)
+    elif args.charlie:
+        decrypt_intercepted(args)
+    elif args.difference:
+        compare_intercepted(args)
     else:
         print "Error, please see usage or --help for assistance"
