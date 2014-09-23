d6c4802b120c712d3198131f8ce76167a37551f5
Matthew Dennison <medennison123@gmail.com>
2014-09-23 11:33:41 -0400

    encrypt and decrypt working

--- caesar.py
+++ caesar.py
@@ -13,7 +13,7 @@ group = parser.add_mutually_exclusive_group()
 group.add_argument("-e", "--encrypt", action="store_true",
        help="Encrypt Alice's plaintext for Bob")
 group.add_argument("-d", "--decrypt", action="store_true",
-       help="Bob decrypting Alice's cyphertext" )
+       help="Decrypt Alice's cyphertext" )
 group.add_argument("-c", "--charlie", action="store_true", 
         help="Charlie's intercepted cyphertext")
 group.add_argument("-diff", "--difference", action="store_true",
@@ -21,13 +21,22 @@ group.add_argument("-diff", "--difference", action="store_true",
 parser.add_argument("-s","--string", 
         help="String to be encrypted or decryped")
 
+
 def encrypt_plaintext(args):
-    print "encrypt works"
-    print args.string
+    message = args.string.lower() 
+    ct = ""
+    key = 3
+    for i in message:
+        ct += chr((((ord(i) - 97) + key)%26) + 97)
+    print "Alice's encrypted message is: " + ct 
 
 def decrypt_cyphertext(args):
-    print "decrypt works"
-    print args.string
+    message = args.string.lower()
+    pt = ""
+    key = 3
+    for i in message:
+        pt += chr((((ord(i) -97) - key)%26) + 97)
+    print "Bob's decrypted message is: " + pt
 
 def decrypt_intercepted(args):
     print "charlie works"
