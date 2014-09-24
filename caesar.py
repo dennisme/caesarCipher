aa04d6a2b4d2cb15e68a4dc78734cc7bb5e2dfaf
Matthew Dennison <medennison123@gmail.com>
2014-09-23 23:52:39 -0400

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -43,7 +43,6 @@ def decrypt_cyphertext(args):
 
 def decrypt_intercepted(args):
     message = args.string.lower()
-    key = 3
     flipped = ""
     binary_ct = bin(int(binascii.hexlify(message), 16))
     print "Charlie's intercepted cyphtext in binary:" + "\n" + binary_ct
@@ -52,7 +51,7 @@ def decrypt_intercepted(args):
         tmp = int(i)
         tmp ^= 1
         flipped += str(tmp)
-    print "Charlie xor the 2th bit to the 12th bit:" "\n" + flipped
+    print "Charlie xor of the 2th bit to the 12th bit:" "\n" + flipped
     
     ctBinary = binary_ct[2:] + flipped + binary_ct[13:(len(binary_ct))]
     tampered_ct = binascii.unhexlify('%x' % (int(ctBinary, 2)))
